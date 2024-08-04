332233ad220ab3a568dddefa9435875d6b378f78
Dennis <ynyz2003@hotmail.com>
2024-08-03 20:24:15 -0500

    ReservationProgressView component added

---
+++ src/components/ReservationProgressView.jsx
@@ -0,0 +1,30 @@
+import { Box, colors, Step, StepLabel, Stepper } from "@mui/material";
+import React from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function ReservationProgressView({ step }) {
+  const steps = ["Trip Details", "Choose a Vehicle", "Confirm"];
+
+  return (
+    <>
+      <Box className="flex flex-col pt-4">
+        <Box className="text-4xl font-bold flex justify-center">
+          Express Ride Reservation Status
+        </Box>
+        <Box className="font-bold">
+          Let us know your ride info and we will take care of your trips.
+        </Box>
+
+        <Box className="w-full pt-10">
+          <Stepper activeStep={step} alternativeLabel>
+            {steps.map((label, i) => (
+              <Step key={i}>
+                <StepLabel componentsProps={{ label: {} }}>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </Box>
+      </Box>
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,32 @@
 import { Box, Button, Grid } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Title from "../components/Title";
+import ReservationProgressView from "../components/ReservationProgressView";
+import TripDetailsForm from "../components/TripDetailsForm";
 
 export default function Home() {
+  const [step, setStep] = useState(0);
+
+  const backStep = () => {
+    if (step == 0) {
+      return;
+    } else {
+      setStep((prev) => prev - 1);
+    }
+  };
+
+  const nextStep = () => {
+    if (step === 3) {
+      return;
+    } else {
+      setStep((prev) => prev + 1);
+    }
+  };
+
+  useEffect(() => {
+    console.log(step);
+  }, [step]);
+
   return (
     <>
       <Box className="mx-10">
@@ -10,6 +34,20 @@ export default function Home() {
           <Grid item xs={12} md={2} />
           <Grid item xs={12} md={8}>
             <Title title={"Express Riding Service"} />
+            <ReservationProgressView step={step} />
+
+            <Box>
+              <TripDetailsForm />
+            </Box>
+
+            <Box className="flex justify-evenly">
+              <Button onClick={() => backStep()} variant="contained">
+                Back
+              </Button>
+              <Button onClick={() => nextStep()} variant="contained">
+                Next
+              </Button>
+            </Box>
           </Grid>
           <Grid item xs={12} md={2} />
         </Grid>
